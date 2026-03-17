e4d3a6dc9442facfadd19199b8af23ffc20a6566
Ahmad Dandeh <ahmad.dandeh7@gmail.com>
2026-03-17 16:42:18 +0100

    Time: 0 ms (100%), Space: 42.3 MB (51.31%) - LeetHub

---
+++ 0868-binary-gap/0868-binary-gap.java
@@ -0,0 +1,15 @@
+class Solution {
+    public int binaryGap(int n) {
+        int ans =0;
+        int last = -1;
+        for(int i=0; i<32; i++){
+            if(((n >> i)&1) == 1){
+                if(last>=0){
+                    ans = Math.max(ans, i-last);
+                }
+                last = i;
+            }
+        }
+        return ans;
+    }
+}

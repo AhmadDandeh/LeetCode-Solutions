da8b519dd5ea9f799b42da2e50307677edb24980
Ahmad Dandeh <ahmad.dandeh7@gmail.com>
2026-03-22 10:21:27 +0100

    Time: N/A (0%), Space: N/A (0%) - LeetHub

---
+++ 1689-partitioning-into-minimum-number-of-deci-binary-numbers/1689-partitioning-into-minimum-number-of-deci-binary-numbers.java
@@ -0,0 +1,9 @@
+class Solution {
+    public int minPartitions(String n) {
+        char ans = '0';
+        for(char c: n.toCharArray()){
+            if(c>ans) ans = c;
+        }
+        return ans - '0';
+    }
+}

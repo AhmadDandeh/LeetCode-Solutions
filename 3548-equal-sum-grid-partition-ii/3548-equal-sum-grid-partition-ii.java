b50fbbaeba5c3f89374ddba316b541d9f89374a2
Ahmad Dandeh <ahmad.dandeh7@gmail.com>
2026-03-26 21:32:19 +0100

    Time: 1101 ms (5.41%), Space: 172.1 MB (97.3%) - LeetHub

--- 3548-equal-sum-grid-partition-ii/3548-equal-sum-grid-partition-ii.java
+++ 3548-equal-sum-grid-partition-ii/3548-equal-sum-grid-partition-ii.java
@@ -2,62 +2,52 @@ class Solution {
     public boolean canPartitionGrid(int[][] grid) {
         int m = grid.length, n = grid[0].length;
         long totalSum = 0;
-        for (int[] row : grid) for (int v : row) totalSum += v;
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) totalSum += grid[i][j];
+        }
 
-        if (m > 1 && check(grid, m, n, totalSum, true)) return true;
-        if (n > 1 && check(grid, n, m, totalSum, false)) return true;
-
-        return false;
-    }
-
-    private boolean check(int[][] grid, int dim1, int dim2, long totalSum, boolean horizontal) {
-        int[] freq1 = new int[100001];
-        int[] freq2 = new int[100001];
-        long sum1 = 0;
-
-        for (int i = 0; i < grid.length; i++) {
-            for (int j = 0; j < grid[0].length; j++) {
-                freq2[grid[i][j]]++;
+        if (m > 1) {
+            long upSum = 0;
+            for (int i = 0; i < m - 1; i++) {
+                for (int j = 0; j < n; j++) upSum += grid[i][j];
+                long downSum = totalSum - upSum;
+                if (check(upSum, downSum, grid, 0, i, 0, n - 1)) return true;
+                if (check(downSum, upSum, grid, i + 1, m - 1, 0, n - 1)) return true;
             }
         }
 
-        for (int i = 0; i < dim1 - 1; i++) {
-            for (int j = 0; j < dim2; j++) {
-                int val = horizontal ? grid[i][j] : grid[j][i];
-                sum1 += val;
-                freq1[val]++;
-                freq2[val]--;
-            }
-
-            long sum2 = totalSum - sum1;
-            if (sum1 == sum2) return true;
-
-            long diff = sum1 - sum2;
-            if (diff > 0) {
-                if (diff <= 100000 && freq1[(int)diff] > 0) {
-                    if (isValid(grid, (int)diff, i + 1, dim2, i, horizontal, true)) return true;
-                }
-            }
-            else {
-                long target = -diff;
-                if (target <= 100000 && freq2[(int)target] > 0) {
-                    if (isValid(grid, (int)target, dim1 - (i + 1), dim2, i + 1, horizontal, false)) return true;
-                }
+        if (n > 1) {
+            long leftSum = 0;
+            for (int j = 0; j < n - 1; j++) {
+                for (int i = 0; i < m; i++) leftSum += grid[i][j];
+                long rightSum = totalSum - leftSum;
+                if (check(leftSum, rightSum, grid, 0, m - 1, 0, j)) return true;
+                if (check(rightSum, leftSum, grid, 0, m - 1, j + 1, n - 1)) return true;
             }
         }
         return false;
     }
 
-    private boolean isValid(int[][] grid, int target, int h, int w, int startIdx, boolean horizontal, boolean isFirstPart) {
-        if (h > 1 && w > 1) return true;
+    private boolean check(long s1, long s2, int[][] grid, int r1, int r2, int c1, int c2) {
+        if (s1 == s2) return true;
+        long diff = s1 - s2;
+        if (diff < 0 || diff > 1000000) return false; 
 
-        if (horizontal) {
-            int r = isFirstPart ? 0 : grid.length - 1;
-            return (grid[r][0] == target || grid[r][grid[0].length - 1] == target);
-        } 
-        else {
-            int c = isFirstPart ? 0 : grid[0].length - 1;
-            return (grid[0][c] == target || grid[grid.length - 1][c] == target);
+        int target = (int) diff;
+        int h = r2 - r1 + 1;
+        int w = c2 - c1 + 1;
+        
+        if (h > 1 && w > 1) {
+            for (int i = r1; i <= r2; i++) {
+                for (int j = c1; j <= c2; j++) {
+                    if (grid[i][j] == target) return true;
+                }
+            }
+        } else if (h == 1) {
+            if (grid[r1][c1] == target || grid[r1][c2] == target) return true;
+        } else if (w == 1) {
+            if (grid[r1][c1] == target || grid[r2][c1] == target) return true;
         }
+        return false;
     }
 }

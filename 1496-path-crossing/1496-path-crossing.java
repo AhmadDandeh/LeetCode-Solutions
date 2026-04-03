c899aa834ef3f0ffd190f4d1d7afb383996e77db
Ahmad Dandeh <ahmad.dandeh7@gmail.com>
2026-04-03 13:13:20 +0200

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 1496-path-crossing/1496-path-crossing.java
+++ 1496-path-crossing/1496-path-crossing.java
@@ -1,15 +1,14 @@
 class Solution {
-    record Point(int i, int j){};
     public boolean isPathCrossing(String path) {
-        HashSet<Point> set = new HashSet<>();
+        HashSet<Integer> set = new HashSet<>();
         int i=0, j=0;
-        set.add(new Point(i,j));
+        set.add(0);
         for(char c: path.toCharArray()){
             if(c == 'N') j++;
             else if(c == 'S') j--;
             else if(c == 'E') i++;
             else i--;
-            if(!set.add(new Point(i,j))) return true;
+            if(!set.add(i*20001+j)) return true;
         }
         return false;
     }

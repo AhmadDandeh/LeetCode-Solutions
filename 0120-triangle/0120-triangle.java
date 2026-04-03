22a952d83339157a8e2a2a677ce92ebb355e9e07
Ahmad Dandeh <ahmad.dandeh7@gmail.com>
2026-04-03 12:58:55 +0200

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0120-triangle/0120-triangle.java
+++ 0120-triangle/0120-triangle.java
@@ -1,5 +1,24 @@
 class Solution {
     public int minimumTotal(List<List<Integer>> triangle) {
+        int n = triangle.size();
+        int[] dp = new int[n];
+
+        for (int i = 0; i < n; i++) {
+            dp[i] = triangle.get(n - 1).get(i);
+        }
+
+        for (int row = n - 2; row >= 0; row--) {
+            for (int col = 0; col <= row; col++) {
+                dp[col] = triangle.get(row).get(col) + Math.min(dp[col], dp[col + 1]);
+            }
+        }
+
+        return dp[0];
+    }
+
+    /*
+    // In-place
+    public int minimumTotal(List<List<Integer>> triangle) {
         for(int i = triangle.size()-2; i>=0; i--){
             for (int j = 0; j <= i; j++) {
                 int bestBelow = Math.min(triangle.get(i + 1).get(j), triangle.get(i + 1).get(j + 1));
@@ -9,4 +28,5 @@ class Solution {
 
         return triangle.get(0).get(0);
     }
+    */
 }

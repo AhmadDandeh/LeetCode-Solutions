27354849c3173cdce93f7c2f5a1d95323d14fd54
Ahmad Dandeh <ahmad.dandeh7@gmail.com>
2026-04-04 11:33:00 +0200

    Time: 3 ms (76.63%), Space: 47.9 MB (40.99%) - LeetHub

--- 0989-add-to-array-form-of-integer/0989-add-to-array-form-of-integer.java
+++ 0989-add-to-array-form-of-integer/0989-add-to-array-form-of-integer.java
@@ -1,30 +1,22 @@
 class Solution {
     public List<Integer> addToArrayForm(int[] num, int k) {
-        List<Integer> ans  = new ArrayList<Integer>();
+        List<Integer> ans  = new ArrayList<>();
         int index = num.length-1;
         int carry = 0;
-        while(k>0 && index>=0){
-            num[index] += (k%10) + carry;
+        while(k>=0 && index>=0){
+            num[index] = num[index] + (k%10) + carry;
             k /= 10;
             carry = (num[index] >=10)? 1:0;
             ans.add(num[index] %10);
             index--;
         }
 
-        while(k>0){
-            k += carry;
-            ans.add(k%10);
-            carry = (k >=10)? 1:0;
+        while(k>0 || carry == 1){
+            int x = (k%10) + carry;
+            ans.add(x%10);
+            carry = (x >=10)? 1:0;
             k/=10;
         }
-
-        while(index>=0){
-            num[index] += carry;
-            ans.add(num[index]%10);
-            carry = (num[index] >=10)? 1:0;
-            index--;
-        }
-        if(carry == 1) ans.add(1);
 
         Collections.reverse(ans);
         return ans;

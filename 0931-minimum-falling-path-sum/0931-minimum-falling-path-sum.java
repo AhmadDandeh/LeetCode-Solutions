dfc0c74e29a27e0f93fc29e55f7d652496a937c1
Ahmad Dandeh <ahmad.dandeh7@gmail.com>
2026-04-09 22:30:07 +0200

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0931-minimum-falling-path-sum/0931-minimum-falling-path-sum.java
+++ 0931-minimum-falling-path-sum/0931-minimum-falling-path-sum.java
@@ -1,23 +1,20 @@
 class Solution {
     public int minFallingPathSum(int[][] matrix) {
-        int m = matrix.length, n = matrix[0].length;
-        if(n == 1){
-            for(int i =1; i<m; i++){
-                matrix[i][0] += matrix[i-1][0];
-            }
-            return matrix[m-1][0];
-        }
-
-        for(int i =1; i<m; i++){
-            matrix[i][0] += Math.min(matrix[i-1][0], matrix[i-1][1]);
-            matrix[i][n-1] += Math.min(matrix[i-1][n-2], matrix[i-1][n-1]);
-            for(int j =1; j<n-1; j++){
-                matrix[i][j] += Math.min(matrix[i-1][j-1], Math.min(matrix[i-1][j], matrix[i-1][j+1]));
+        int n = matrix.length;
+    
+        for (int i = 1; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                int up = matrix[i-1][j];
+                int leftUp = (j > 0) ? matrix[i-1][j-1] : Integer.MAX_VALUE;
+                int rightUp = (j < n - 1) ? matrix[i-1][j+1] : Integer.MAX_VALUE;
+                
+                matrix[i][j] += Math.min(up, Math.min(leftUp, rightUp));
             }
         }
-        int min = matrix[m-1][0];
-        for(int j =1; j<n; j++){
-            min = Math.min(min, matrix[m-1][j]);
+        
+        int min = Integer.MAX_VALUE;
+        for (int val : matrix[n-1]) {
+            min = Math.min(min, val);
         }
         return min;
     }

dab1dc01b1483df67132905ccc015e258dbcb81a
Ahmad Dandeh <ahmad.dandeh7@gmail.com>
2026-04-10 12:16:21 +0200

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 3740-minimum-distance-between-three-equal-elements-i/3740-minimum-distance-between-three-equal-elements-i.java
+++ 3740-minimum-distance-between-three-equal-elements-i/3740-minimum-distance-between-three-equal-elements-i.java
@@ -9,9 +9,9 @@ class Solution {
         for (Map.Entry<Integer, List<Integer>> entry : map.entrySet()) {
             if(entry.getValue().size() < 3) continue;
             for(int j = 2; j < entry.getValue().size(); j++){
-                min = Math.min(min, 2 * (entry.getValue().get(j) - entry.getValue().get(j - 2)));
+                min = Math.min(min, entry.getValue().get(j) - entry.getValue().get(j - 2));
             }
         }
-        return min ==  Integer.MAX_VALUE ? -1 : min;
+        return min ==  Integer.MAX_VALUE ? -1 : 2 * min;
     }
 }

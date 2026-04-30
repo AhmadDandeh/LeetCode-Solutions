b9c9e1dabcc34b839f6e125eb4fc8adf912aef3e
Ahmad Dandeh <ahmad.dandeh7@gmail.com>
2026-04-30 12:19:39 +0200

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 2194-cells-in-a-range-on-an-excel-sheet/2194-cells-in-a-range-on-an-excel-sheet.java
+++ 2194-cells-in-a-range-on-an-excel-sheet/2194-cells-in-a-range-on-an-excel-sheet.java
@@ -1,5 +1,14 @@
 class Solution {
     public List<String> cellsInRange(String s) {
-        
+        char colS = s.charAt(0), colE = s.charAt(3);
+        char rowS = s.charAt(1), rowE = s.charAt(4);
+
+        List<String> ans = new ArrayList<>();
+        for(char i = colS; i<= colE; i++){
+            for(char j = rowS; j<= rowE; j++){
+                ans.add(i+""+j);
+            }
+        }
+        return ans;
     }
 }

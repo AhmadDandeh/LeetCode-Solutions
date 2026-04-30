12255fd4cbab169a8fba1105c26dcfe03371d3fa
Ahmad Dandeh <ahmad.dandeh7@gmail.com>
2026-04-30 12:16:34 +0200

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0448-find-all-numbers-disappeared-in-an-array/0448-find-all-numbers-disappeared-in-an-array.java
+++ 0448-find-all-numbers-disappeared-in-an-array/0448-find-all-numbers-disappeared-in-an-array.java
@@ -1,9 +1,18 @@
 class Solution {
     public List<Integer> findDisappearedNumbers(int[] nums) {
+        boolean[] valid = new boolean[nums.length+1];
+        for(int num:nums) valid[num] = true;
+        List<Integer> ans = new ArrayList<>();
+        for(int i =1; i< valid.length; i++) if(!valid[i]) ans.add(i);
+        return ans;
+    }
+    /*
+    public List<Integer> findDisappearedNumbers(int[] nums) {
         HashSet<Integer> set = new HashSet<>();
         for(int n:nums) set.add(n);
         List<Integer> ans = new LinkedList<>();
         for(int i =1; i<=nums.length; i++) if(!set.contains(i)) ans.add(i);
         return ans;
     }
+    */
 }

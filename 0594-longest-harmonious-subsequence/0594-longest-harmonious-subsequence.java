d3d6df99d736e43642e938cbcef379e4fff80d16
Ahmad Dandeh <ahmad.dandeh7@gmail.com>
2026-05-03 12:34:19 +0200

    Time: N/A (0%), Space: N/A (0%) - LeetHub

---
+++ 0594-longest-harmonious-subsequence/0594-longest-harmonious-subsequence.java
@@ -0,0 +1,12 @@
+class Solution {
+    public int findLHS(int[] nums) {
+        Arrays.sort(nums);
+        int ans = 0, left =0;
+        int n = nums.length;
+        for(int right =0; right<n; right++){
+            if(nums[right] - nums[left] > 1) left++;
+            if(nums[right] == nums[left]+1) ans = Math.max(ans, right-left+1);
+        }
+        return ans;
+    }
+}

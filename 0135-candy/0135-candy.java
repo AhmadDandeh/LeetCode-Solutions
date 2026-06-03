6a2773e84c85de5e792359375d77191a56e5402b
Ahmad Dandeh <ahmad.dandeh7@gmail.com>
2026-06-03 13:55:27 +0200

    Time: 3 ms (88.73%), Space: 47.3 MB (95.09%) - LeetHub

---
+++ 0135-candy/0135-candy.java
@@ -0,0 +1,15 @@
+class Solution {
+    public int candy(int[] ratings) {
+        int n = ratings.length;
+        int[] arr = new int[n];
+        for(int i =1; i<n; i++){
+            if(ratings[i] > ratings[i-1]) arr[i] = arr[i-1]+1;
+        }
+        int ans = n + arr[n-1];
+        for(int i =n-2; i>=0; i--){
+            if(ratings[i] > ratings[i+1]) arr[i] = Math.max(arr[i], arr[i+1]+1);
+            ans += arr[i];
+        }
+        return ans;
+    }
+}

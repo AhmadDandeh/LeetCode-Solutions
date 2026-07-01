4622707276a1d883276403b1c32305f77d76517a
Ahmad Dandeh <ahmad.dandeh7@gmail.com>
2026-07-01 23:45:58 +0200

    Time: 74 ms (58.47%), Space: 46.9 MB (52.93%) - LeetHub

--- 0459-repeated-substring-pattern/0459-repeated-substring-pattern.java
+++ 0459-repeated-substring-pattern/0459-repeated-substring-pattern.java
@@ -4,11 +4,6 @@ class Solution {
      * We can solve it using LPS
      */
     public boolean repeatedSubstringPattern(String s) {
-        StringBuilder sb = new StringBuilder();
-        sb.append(s);
-        sb.append(s);
-        sb.deleteCharAt(0);
-        sb.deleteCharAt(sb.length()-1);
-        return sb.toString().indexOf(s) != -1;
+        return (s+s).substring(1,s.length()*2-1).contains(s);
     }
 }
